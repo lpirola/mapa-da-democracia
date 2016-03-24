beac33e3ca994cdf41607018f0e282ac40e805f2
Maurilio Atila <cabelotaina@gmail.com>
2016-03-24 11:09:43 -0300

    remove console log

--- src/components/ParlamentarCard.js
+++ src/components/ParlamentarCard.js
@@ -6,7 +6,6 @@ const { Button, Alert, Card, Row, Col, Glyph } = require('elemental');
 
 const ParlamentarCard = (props) => {
   let data = props.data[0];
-  console.log(props.data[0]);
 //  console.log(data);
   const handleChange = (e) => {
     props.onChange(props.name, e.target.value);
