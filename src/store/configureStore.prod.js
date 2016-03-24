b8cf5464a14b62aa03942528706902b103e7fca3
Lucas Pirola <lucaspirola@gmail.com>
2016-03-24 01:01:20 -0300

    Corrigi bug com configstore em modo de producao

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers';
+import thunkMiddleware from 'redux-thunk';
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState);
+  return createStore(rootReducer, initialState, compose(
+      applyMiddleware(thunkMiddleware)
+    )
+  );
 }
